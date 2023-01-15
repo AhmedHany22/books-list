b203abcf7655e9c0b8e16595cfa6356e44fce43a
Ahmed <ahmed_hany19@outlook.com>
2023-01-15 18:20:36 +0200

    Applying Context

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import useBooksContext from '../hooks/use-books-context';
 
 function BookEdit({ book, onSubmit }) {
   const [title, setTitle] = useState(book.title);
+  const { editBookById } = useBooksContext();
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -10,7 +12,8 @@ function BookEdit({ book, onSubmit }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    onSubmit(book.id, title);
+    onSubmit();
+    editBookById(book.id, title);
   };
 
   return (
